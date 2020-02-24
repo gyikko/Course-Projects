327b9b29b5ff869ce395b717239fdf0e43ffabf4
jinxx679 <jinxx679@umn.edu>
2020-02-24 12:28:02 -0600

    Add lab08

--- labs/lab08_google_tests/date_unittest.cc
+++ labs/lab08_google_tests/date_unittest.cc
@@ -5,7 +5,7 @@
 #include <ctime>
 
 #include "date.h"
-
+using namespace std;
 
 class DateTest : public ::testing::Test {
  public:
@@ -19,6 +19,44 @@ class DateTest : public ::testing::Test {
   Date last_day;           // last day of classes
 };
 
+TEST_F(DateTest, dateConstructorTest) {
+  Date testEpoch(0); /*
+  std::time_t epoch_t = 0;
+  std::tm* yyyy_mm_dd = std::gmtime( &epoch_t );
+  
+  //tm year's value is years since 1900
+  int yyyy_ = yyyy_mm_dd->tm_year + 1970;
+  //tm month's value is zero-indexed
+  int mm_ = yyyy_mm_dd->tm_mon + 1;
+  //tm day's value is one-indexed
+  int dd_ = yyyy_mm_dd->tm_mday;
+  */
+  EXPECT_EQ(testEpoch.GetDate(), "1970-01-01") << "Date Constructor of class not setup properly";
+}
+
+TEST_F(DateTest, operatePlusTest) {
+  Date new_first = first_day.operator+(1);
+  Date new_last = last_day.operator+(1);
+  EXPECT_EQ(new_first.GetDate(), "2018-09-05") << "operator- not setup properly";
+  EXPECT_EQ(new_last.GetDate(), "2018-12-12") << "operator- not setup properly";
+}
+
+TEST_F(DateTest, operateMinusTest) {
+  Date new_first = first_day.operator-(1);
+  Date new_last = last_day.operator-(1);
+  EXPECT_EQ(new_first.GetDate(), "2018-09-03") << "operator- not setup properly";
+  EXPECT_EQ(new_last.GetDate(), "2018-12-10") << "operator- not setup properly";
+}
+
+TEST_F(DateTest,GetDateTest) {
+  EXPECT_EQ(first_day.GetDate(), "2018-09-04") << "First day of class not setup properly";
+  EXPECT_EQ(last_day.GetDate(), "2018-12-11") << "Last day of class not setup properly";
+}
+
+TEST_F(DateTest, GetUsDateTest) {
+  EXPECT_EQ(first_day.GetUsDate(), "09-04-2018") << "First day of class not setup properly";
+  EXPECT_EQ(last_day.GetUsDate(), "12-11-2018") << "Last day of class not setup properly";
+}
 
 
 TEST_F(DateTest, PrintDateTests) {
