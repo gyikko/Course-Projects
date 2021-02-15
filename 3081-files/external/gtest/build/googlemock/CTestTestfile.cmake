5666e37908a30d4d0b49564cbc86522f0e68a8a1
Jingyi Jin <jinxx679@umn.edu>
2021-02-14 18:09:02 -0600

    remove 3081 code

--- 3081-files/external/gtest/build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jinxx679/Documents/3081/repo-jinxx679/external/gtest/src/googlemock
-# Build directory: /home/jinxx679/Documents/3081/repo-jinxx679/external/gtest/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 3081-files/labs/lab03_class_basics/point2.cc
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <time.h> // used for seeding rand()
-#include <cstdlib>
-#include "point2.h"
-
